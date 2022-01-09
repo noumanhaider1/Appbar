2abc7de6c541e4aa0c097d6e47dc0e4aa20ea4b8
Nouman Haider <noman.haider10@gmail.com>
2022-01-10 01:00:35 +0500

    start code

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to My App "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
